b9128b6b481190d260979fdaa0b56c8fd46e85f7
kristofferskvam <kristoffer.seth.kvam@gmail.com>
2024-10-01 13:47:00 +0200

    Lab0

---
+++ basic.py
@@ -0,0 +1,21 @@
+num1 = 1
+num2 = 2
+print(num1+num2)
+
+print(num1*num2)
+
+print(num1/num2)
+
+name = "Kristoffer"
+age = 23
+
+print(name + " is " + str(age) + " years old")
+
+numList = [2, 3, 5, 7, 11]
+for x in numList:
+    print(x)
+num3 = 7
+if (num1 > 5):
+    print(str(num3)+" is bigger then 5")
+else:
+    print(str(num3)+" is not bigger then 5")
